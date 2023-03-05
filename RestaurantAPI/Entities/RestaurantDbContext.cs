acf4746c1bf1daebcae66e917a0bc925f4a4e44e
Piotr Wisniewski <piowisni@gmail.com>
2023-03-05 18:51:13 +0100

    Db created

---
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+    }
+}
--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using System;
+
 namespace RestaurantAPI
 {
     public class Program
@@ -11,6 +15,8 @@ namespace RestaurantAPI
             builder.Services.AddControllers();
             builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
 
+            builder.Services.AddDbContext<RestaurantDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
